ef1aedaed4ed48a9e72b2683f939c3ff54f345cd
HAJINYOUNG <jyha@deleo.co.kr>
2025-03-11 16:31:35 +0900

    [UPDATE] karpenter_version 변수 기본값 추가 및 terraform.tfvars 파일 삭제 ✨

--- variables.tf
+++ variables.tf
@@ -56,6 +56,7 @@ variable "cluster_primary_security_group_id" {
 variable "karpenter_version" {
   description = "karpenter version for install crd"
   type        = string
+  default     = "0.37.0"
 }
 variable "tags" {
   description = "tags"
