6f2fc7a202a802b9bce6cf42bc6e37d8020eced4
HAJINYOUNG <jyha@deleo.co.kr>
2025-04-18 11:21:47 +0900

    [ADD] ENIConfig 템플릿 및 VPC CNI 값 업데이트 ✨

--- 01_vpc_cni.tf
+++ 01_vpc_cni.tf
@@ -8,7 +8,7 @@ resource "helm_release" "eni_config" {
   repository = "https://bedag.github.io/helm-charts/"
   chart      = "raw"
   version    = "2.0.0"
-  values = [ templatefile(".terraform/modules/eks_init/eni_config.tftpl", {
+  values = [ templatefile("${path.module}/templates/eni_config.tftpl", {
     eni_configs = zipmap(var.azs, var.eks_pod_subnet_ids),
     securityGroups = [
       var.cluster_primary_security_group_id
