6377c4f9bdcf891bc290a38c4cbe793166517ca6
HAJINYOUNG <jyha@deleo.co.kr>
2025-05-26 14:56:15 +0900

    [UPDATE] 변수 선언 및 기본값 추가 및 수정 ✏️ - AWS 관련 변수 선언 구조 및 설명 수정 - 기본값 추가 및 타입 정의 개선 - 새로운 변수(Coredns, VPC CNI, Kube Proxy, Metrics Server) 추가

--- variables.tf
+++ variables.tf
@@ -1,65 +1,155 @@
-variable  "profile" {
-  description = "credential profile"
-  type        = any
-  default     = {}
+################################################################################
+# AWS Region
+################################################################################
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-2"
 }
+
+################################################################################
+# AWS Profile
+################################################################################
+variable "profile" {
+  description = "AWS credential profile"
+  type        = string
+  default     = "deleoec"
+}
+
+################################################################################
+# AWS Shared Credentials Files
+################################################################################
 variable "shared_credentials_files" {
-  description = "shared credentials files"
-  type        = any
-  default     = {}
+  description = "AWS shared credentials files"
+  type        = list(string)
+  default     = ["~/.aws/credentials"]
 }
-variable  "azs" {
-  description = "aws_availability_zones"
-  type        = any
-  default     = {}
+
+################################################################################
+# AWS Availability Zones
+################################################################################
+variable "azs" {
+  description = "AWS availability zones"
+  type        = list(string)
+  default     = []
 }
+
+################################################################################
+# EKS Pod Subnet IDs
+################################################################################
 variable "eks_pod_subnet_ids" {
-  description = "eks pod subnet ids"
-  type        = any
-  default     = {}
+  description = "EKS Pod Subnet IDs"
+  type        = list(string)
+  default     = []
 }
+
+################################################################################
+# EKS Cluster
+################################################################################
 variable "cluster_name" {
-  description = "cluster name"
-  type        = any
-  default     = {}
+  description = "EKS cluster name"
+  type        = string
 }
+
+################################################################################
+# EKS Cluster Endpoint
+################################################################################
 variable "cluster_endpoint" {
-  description = "cluster endpoint"
-  type        = any
-  default     = {}
+  description = "EKS cluster endpoint"
+  type        = string
 }
+
+################################################################################
+# EKS Cluster Version
+################################################################################
 variable "cluster_version" {
-  description = "cluster version"
-  type        = any
-  default     = {}
+  description = "EKS cluster version"
+  type        = string
 }
+
+################################################################################
+# OIDC Provider ARN
+################################################################################
 variable "oidc_provider_arn" {
-  description = "oidc provider arn"
-  type        = any
-  default     = {}
+  description = "OIDC provider ARN"
+  type        = string
 }
+
+################################################################################
+# Fargate Profiles
+################################################################################
 variable "fargate_profiles" {
-  description = "fargate profiles"
-  type        = any
+  description = "Fargate profiles"
+  type        = map(any)
   default     = {}
 }
+
+################################################################################
+# EKS Cluster CA Certificate
+################################################################################
 variable "cluster_ca_certificate" {
-  description = "cluster ca certificate"
-  type        = any
-  default     = {}
+  description = "EKS cluster CA certificate"
+  type        = string
 }
+
+################################################################################
+# EKS Cluster Primary Security Group ID
+################################################################################
 variable "cluster_primary_security_group_id" {
-  description = "cluster primary security group id"
-  type        = any
-  default     = {}
+  description = "EKS cluster primary security group ID"
+  type        = string
 }
+
+################################################################################
+# Karpenter Version
+################################################################################
 variable "karpenter_version" {
-  description = "karpenter version for install crd"
-  type        = any
+  description = "Karpenter version for install CRD"
+  type        = string
+  default     = "0.37.0"
+}
+
+################################################################################
+# Tags
+################################################################################
+variable "tags" {
+  description = "Tags"
+  type        = map(string)
   default     = {}
 }
-variable "tags" {
-  description = "tags"
-  type        = any
-  default     = {}
+
+################################################################################
+# CoreDNS Version
+################################################################################
+variable "coredns_version" {
+  description = "CoreDNS version"
+  type        = string
+  default     = "v1.11.4-eksbuild.2"
+}
+
+################################################################################
+# VPC CNI Version
+################################################################################
+variable "vpc_cni_version" {
+  description = "VPC CNI version"
+  type        = string
+  default     = "v1.19.3-eksbuild.1"
+}
+
+################################################################################
+# Kube Proxy Version
+################################################################################
+variable "kube_proxy_version" {
+  description = "Kube Proxy version"
+  type        = string
+  default     = "v1.19.3-eksbuild.1"
+}
+
+################################################################################
+# Metrics Server Version
+################################################################################
+variable "metrics_server_version" {
+  description = "Metrics Server version"
+  type        = string
+  default     = "v1.19.3-eksbuild.1"
 }
