d32800a8a92b9045a6f4c8158a0ba94205a1d7a4
chan <bcchoi@deleo.co.kr>
2025-10-27 14:01:44 +0900

    fix: missing karpenter variable added

--- variables.tf
+++ variables.tf
@@ -13,6 +13,11 @@ variable  "azs" {
   type        = any
   default     = {}
 }
+variable "karpenter" {
+  description = "karpenter add-on configuration values"
+  type        = any
+  default     = {}
+}
 variable  "karpenter_azs" {
   description = "karpenter default nodepool aws_availability_zones"
   type        = any
